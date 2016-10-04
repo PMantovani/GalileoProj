55a16227d45a0dc65e4fde590a60dca6b961bf1f
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-10-04 19:50:09 -0300

    Fix time format

--- server1.py
+++ server1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import SimpleHTTPServer
-import SocketServer
+from SocketServer import ThreadingMixIn
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
 import sys
 import datetime
@@ -19,7 +20,7 @@ LUM_PIN = A1
 
 # Handler used for handling HTTP requests
 class PersistentHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    protocol_version = "HTTP/1.1"
+    pass
 
 # Thread to update index.html with new sensor readings
 class UpdateSensorValues(Thread):
@@ -34,7 +35,7 @@ class UpdateSensorValues(Thread):
             # Get current time
             n = datetime.datetime.now()
             # Format time
-            timeStr = "%2.d:%2.d:%2.d %d/%d/%d" % (n.hour, n.minute, n.second, n.day, n.month, n.year)
+            timeStr = "%02d:%02d:%02d %d/%d/%d" % (n.hour, n.minute, n.second, n.day, n.month, n.year)
 
             # Replace strings in indexRaw.html file to index.html
             fileStr = fileStr.replace("$hora", timeStr)
@@ -49,8 +50,12 @@ class UpdateSensorValues(Thread):
             # delay for update
             delay(5000)
 
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    # Handles requests in a separate thread.
+    pass
+
 Handler = PersistentHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd = ThreadedHTTPServer(("", PORT), Handler)
 httpd.allow_reuse_address = True
 
 # Runs new thread
