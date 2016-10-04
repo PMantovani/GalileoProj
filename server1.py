fe6b8bbcb2b790455d32ac2c6179b9672ab73344
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-10-04 17:11:05 -0300

    Ended method for reading sensor values

--- server1.py
+++ server1.py
@@ -3,6 +3,7 @@ import SimpleHTTPServer
 import SocketServer
 import threading
 import sys
+import datetime
 
 # Include I/O library for python in PYTHON PATH
 galileo_path = ""
@@ -24,6 +25,24 @@ class PersistentHandler(SimpleHTTPRequestHandler):
 def UpdateSensorValues(threading.thread):
     temperature = analogRead(TEMP_PIN)
     luminosity = analogRead(LUM_PIN)
+    file = open('indexRaw.html', 'r')
+    fileStr = file.read()
+    file.close()
+
+    # Get current time
+    n = datetime.datetime.now()
+    # Format time
+    timeStr = "%d:%d:%d %d/%d/%d" % (n.hour, n.minute, n.second, n.day, n.month, n.year)
+
+    # Replace strings in indexRaw.html file to index.html
+    fileStr.replace("$hora", timeStr)
+    fileStr.replace("$temp", string(temperature))
+    fileStr.replace("$lum", string(luminosity))
+
+    # Write changes to index.html
+    fileWrite = open("index.html", "w+")
+    fileWrite.write(fileStr)
+    fileWrite.close()
 
 Handler = PersistentHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
