fe9a1782cd4ae6435d8aba641aace1db253bff42
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-10-04 16:39:25 -0300

    Include pyGalileo library and thread to read sensor values

--- server1.py
+++ server1.py
@@ -1,11 +1,38 @@
+#!/usr/bin/env python
 import SimpleHTTPServer
 import SocketServer
+import threading
+import sys
+
+# Include I/O library for python in PYTHON PATH
+galileo_path = ""
+if galileo_path not in sys.path:
+    sys.path.append(galileo_path);
+
+import pyGalileo
+
 
 PORT = 8000
+TEMP_PIN = 666666
+LUM_PIN = 66666666
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+# Handler used for handling HTTP requests
+class PersistentHandler(SimpleHTTPRequestHandler):
+    protocol_version = "HTTP/1.1"
 
+# Thread to update index.html with new sensor readings
+def UpdateSensorValues(threading.thread):
+    temperature = analogRead(TEMP_PIN)
+    luminosity = analogRead(LUM_PIN)
+
+Handler = PersistentHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+# Defines sensor pins as inputs
+pinMode(TEMP_PIN, INPUT)
+pinMode(LUM_PIN, INPUT)
+# Runs new thread
+UpdateSensorValues().start()
 
 print "serving at port", PORT
 httpd.serve_forever()
