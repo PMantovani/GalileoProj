ef710531fedcb60c405a475839c4c9921f842dea
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-10-04 17:47:40 -0300

    Fix a lot of bugs

--- server1.py
+++ server1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import SimpleHTTPServer
 import SocketServer
-import threading
+from threading import Thread
 import sys
 import datetime
 
@@ -10,7 +10,7 @@ galileo_path = "/var/project"
 if galileo_path not in sys.path:
     sys.path.append(galileo_path);
 
-import pyGalileo
+from pyGalileo import *
 
 
 PORT = 8000
@@ -18,38 +18,40 @@ TEMP_PIN = A0
 LUM_PIN = A1
 
 # Handler used for handling HTTP requests
-class PersistentHandler(SimpleHTTPRequestHandler):
+class PersistentHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     protocol_version = "HTTP/1.1"
 
 # Thread to update index.html with new sensor readings
-def UpdateSensorValues(threading.thread):
-    temperature = analogRead(TEMP_PIN)
-    luminosity = analogRead(LUM_PIN)
-    file = open('index_std.html', 'r')
-    fileStr = file.read()
-    file.close()
+def UpdateSensorValues(Thread):
+    def run():
+        while (true):
+            temperature = analogRead(TEMP_PIN)
+            luminosity = analogRead(LUM_PIN)
+            file = open('index_std.html', 'r')
+            fileStr = file.read()
+            file.close()
 
-    # Get current time
-    n = datetime.datetime.now()
-    # Format time
-    timeStr = "%d:%d:%d %d/%d/%d" % (n.hour, n.minute, n.second, n.day, n.month, n.year)
+            # Get current time
+            n = datetime.datetime.now()
+            # Format time
+            timeStr = "%d:%d:%d %d/%d/%d" % (n.hour, n.minute, n.second, n.day, n.month, n.year)
 
-    # Replace strings in indexRaw.html file to index.html
-    fileStr.replace("$hora", timeStr)
-    fileStr.replace("$temp", string(temperature))
-    fileStr.replace("$lum", string(luminosity))
+            # Replace strings in indexRaw.html file to index.html
+            fileStr.replace("$hora", timeStr)
+            fileStr.replace("$temp", string(temperature))
+            fileStr.replace("$lum", string(luminosity))
 
-    # Write changes to index.html
-    fileWrite = open("index.html", "w+")
-    fileWrite.write(fileStr)
-    fileWrite.close()
+            # Write changes to index.html
+            fileWrite = open("index.html", "w+")
+            fileWrite.write(fileStr)
+            fileWrite.close()
+
+            # delay for update
+            delay(5000)
 
 Handler = PersistentHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-# Defines sensor pins as inputs
-pinMode(TEMP_PIN, INPUT)
-pinMode(LUM_PIN, INPUT)
 # Runs new thread
 UpdateSensorValues().start()
 
