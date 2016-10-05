8360ad95dc176bc6b36aa65a9c8569a7a7e53221
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-10-04 22:06:13 -0300

    Add server for part 2

---
+++ logserver.py
@@ -0,0 +1,22 @@
+import SocketServer
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+
+        with open("log.txt", "a") as file:
+            writeStr = "\n%s" % self.data
+            file.write(writeStr)
+
+if __name__ == "__main__":
+    HOST, PORT = "", 4444
+
+    # Create the server, binding to localhost on port 4444
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
