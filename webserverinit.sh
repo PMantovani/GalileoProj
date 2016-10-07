d3ad62de97f3f70cbdc434edc33383d5efa83bde
Erik Nayan <eriknayan@gmail.com>
2016-10-07 02:01:21 -0300

    priority added on system boot

--- webserverinit.sh
+++ webserverinit.sh
@@ -3,5 +3,6 @@
 killall lighttpd
 ifconfig enp0s20f6 192.168.25.32 netmask 255.255.255.0 up
 cd /var/project/
-python logclient.py &
-python simple_server.py &
+nice -n -11 python logclient.py &
+nice -n -1  python simple_server.py &
+#não sei se vai funcionar, mas teoricamente isso vai iniciar o processo setando essas prioridades.
