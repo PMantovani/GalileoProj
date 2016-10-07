3d3c619329f25b6548a405aa85c57ab63e1a66e4
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-10-07 11:19:01 -0300

    Fix the timezone on boot

--- webserverinit.sh
+++ webserverinit.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Arruma o fuso horário
+export TZ=America/Sao_Paulo
+# Mata o servidor http padrão embarcado
 killall lighttpd
+# Sobe com o endereço IP fixo
 ifconfig enp0s20f6 192.168.25.32 netmask 255.255.255.0 up
+# Roda o servidor web e o cliente de log
 cd /var/project/
 nice -n -11 python logclient.py &
 nice -n -1  python simple_server.py &
